1dc4c0cad4eb8c3be88ad4b8246a0efd0fe1e470
shiafrahman <shiafrahman@gmail.com>
2025-05-20 11:10:50 +0600

    ffff

---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly EnterpriseDbContext _context;
+
+        public UnitOfWork(EnterpriseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
